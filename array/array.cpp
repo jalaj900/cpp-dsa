8166dea3d1ac3fee6171df4660fdd25cebb87051
Your Name <sahaijalaj16@gmail.com>
2021-07-04 00:43:39 +0530

    create ,search and sort array

---
+++ array/array.cpp
@@ -0,0 +1,38 @@
+//array create
+//linear search
+
+#include <iostream>
+using namespace std;
+
+int  linear(int arr[], int n, int key)
+{
+    for (int i = 0; i <n; i++)
+    {
+        if (arr[i]==key){
+            return i;
+        }
+        return -1;
+    }
+    
+}   
+
+int main()
+{
+    int n;
+    cout << "enter size of array";
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+ 
+    }
+return 0;
+}
+
