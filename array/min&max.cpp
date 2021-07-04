41307b5e86250778ae72566fb78668a11feb11e5
Jalaj Sahai <sahaijalaj16@gmail.com>
2021-07-04 11:22:24 +0530

    done min and max element

---
+++ array/min&max.cpp
@@ -0,0 +1,32 @@
+//max and min element in an aaray
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a[n];
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int max = a[0];
+    int min = a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+
+        if (a[i]<min){
+            min=a[i];
+        }
+    }
+
+    cout <<max<<" "<<min;
+}
