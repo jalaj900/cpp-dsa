42900ac002b712d757c6adb923a84851f1afa3e9
Jalaj Sahai <sahaijalaj16@gmail.com>
2021-07-12 17:06:09 +0530

    done bubble and selection

---
+++ sorting_tech/bubble.cpp
@@ -0,0 +1,41 @@
+//bubble sort
+//repeatedly swap two adjacent
+//elements if they are in wrong order
+//wrong order => left>right
+//we have to do n-1 iteration to get sorted array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int counter = 1;
+
+    while (counter < n - 1)
+    {
+        for (int i = 0; i < n - counter; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        counter++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
