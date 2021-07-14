bf532e2aa8d5d20da34af262d3dfa4e5a05b2cb7
jalaj900 <67186159+jalaj900@users.noreply.github.com>
2021-07-14 23:51:13 +0530

    done wave sort

---
+++ sorting_tech/wave.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void wave_sort(int arr[], int n)
+{
+
+    for (int i = 1; i < n - 1; i += 2)
+    {
+        if (arr[i] > arr[i - 1])
+        {
+            swap(arr, i, i - 1);
+        }
+
+        if (arr[i]>arr[i+1] && i<=n-2){
+            swap(arr,i,i+1);
+        }
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    wave_sort(arr,n);
+
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }
+}
