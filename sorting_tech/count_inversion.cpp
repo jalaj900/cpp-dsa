0da00b08473eb42b5ddbf8614a4deab133c22785
jalaj900 <67186159+jalaj900@users.noreply.github.com>
2021-07-15 00:28:19 +0530

    Add files via upload

---
+++ sorting_tech/count_inversion.cpp
@@ -0,0 +1,78 @@
+//A: [3, 5, 6, 9, 1, 2, 7, 8]
+//Inversions: 10
+//Explanation: (3,1), (3,2), (5,1), (5,2), (6,1), (6,2), (9,1), (9,2), (9,7), (9,8)
+//time complexity= O(n logn)
+//this code is basically merge sort with some modification
+
+#include "bits/stdc++.h"
+using namespace std;
+long long merge(int arr[], int l, int mid, int r)
+{
+    long long inv = 0;
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+    int a[n1];
+    int b[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        a[i] = arr[l + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        b[i] = arr[mid + i + 1];
+    }
+    int i = 0, j = 0, k = l;
+    while (i < n1 and j < n2)
+    {
+        if (a[i] <= b[j])
+        {
+            arr[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            arr[k] = b[j];
+            inv += n1 - i; // modification merge sort
+            k++;
+            j++;
+        }
+    }
+    while (i < n1)
+    {
+        arr[k] = a[i];
+        k++;
+        i++;
+    }
+    while (j < n2)
+    {
+        arr[k] = b[j];
+        k++;
+        j++;
+    }
+    return inv;
+}
+long long mergeSort(int arr[], int l, int r)
+{
+    long long inv = 0;
+    if (l < r)
+    {
+        int mid = (l + r) / 2;
+        inv += mergeSort(arr, l, mid);         // modification merge sort
+        inv += mergeSort(arr, mid + 1, r);     // modification merge sort
+        inv += merge(arr, l, mid, r);          // modification merge sort
+    }
+    return inv;
+}
+int32_t main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << mergeSort(arr, 0, n - 1);
+    return 0;
+}
