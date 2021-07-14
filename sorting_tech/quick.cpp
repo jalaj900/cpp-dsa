ca10e2a761d8611140704dd2faf1b5f691e44d12
jalaj900 <67186159+jalaj900@users.noreply.github.com>
2021-07-14 23:53:20 +0530

    done merge ,quick,insertion sort

---
+++ sorting_tech/quick.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r];
+    int i = l - 1;
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, r);
+    return i+1;
+}
+
+//quick sort function
+
+void quick_sort(int arr[],int l, int r){
+    if(l<r){
+        int pi=partition(arr,l,r);
+        quick_sort(arr,l,pi-1);
+        quick_sort(arr,pi+1,r);
+    }
+}
+ 
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    //call quick sort function
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
