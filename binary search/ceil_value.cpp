ff25817dd233623c02bd2a13b0c5fc083710cde0
jalaj900 <67186159+jalaj900@users.noreply.github.com>
2021-09-14 01:57:15 +0530

    Add files via upload

---
+++ binary search/ceil_value.cpp
@@ -0,0 +1,54 @@
+
+//binary search
+
+#include <iostream>
+using namespace std;
+
+int binary_search(int arr[], int ele, int n)
+{
+    int start = 0;
+    int end = n - 1;
+    int res=0;
+
+    while (start <= end)
+    {
+
+        int mid = start + (end - start) / 2;    // this prevents int overflow
+
+        if (ele == arr[mid])
+        {
+            return mid;
+        }
+
+        else if (ele < arr[mid])
+        {
+            res= arr[mid];   // modifiy bs here to get the solution
+    
+            end = mid - 1;
+        }
+        else
+        {
+            
+            start = mid + 1;
+        }
+ 
+    }
+    return res;
+
+}
+
+int main()
+{
+
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int ele;
+    cin>>ele;
+    cout<<binary_search(arr,ele,n);
+}
---
+++ binary search/floor_value_of_ele.cpp
@@ -0,0 +1,52 @@
+
+//binary search
+
+#include <iostream>
+using namespace std;
+
+int binary_search(int arr[], int ele, int n)
+{
+    int start = 0;
+    int end = n - 1;
+    int res=0;
+
+    while (start <= end)
+    {
+
+        int mid = start + (end - start) / 2;    // this prevents int overflow
+
+        if (ele == arr[mid])
+        {
+            return mid;
+        }
+
+        else if (ele < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            res= arr[mid];
+            start = mid + 1;
+        }
+ 
+    }
+    return res;
+
+}
+
+int main()
+{
+
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int ele;
+    cin>>ele;
+    cout<<binary_search(arr,ele,n);
+}
